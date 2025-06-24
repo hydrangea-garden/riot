3746368e7e6d33bf8c29ab5073fcece346875555
Ryu Juheon <saidbysolo@gmail.com>
2025-06-24 22:50:23 +0900

    feat: 예외처리

--- riot/server.py
+++ riot/server.py
@@ -1,5 +1,5 @@
 from types import SimpleNamespace
-from typing import Any
+from typing import Any, Mapping, Sequence
 
 from sanic import Request, Sanic
 from sanic.response import json
@@ -32,21 +32,28 @@ async def get_info(request: ServerRequest):
     account = Account(request.app.ctx.riot_api)
     league = League(request.app.ctx.riot_api)
     summoner = Summoner(request.app.ctx.riot_api)
-    response = await account.get_account_by_game_name_and_tag(
-        game_name=request.json.get("game_name", ""),
-        tag_line=request.json.get("tag_line", ""),
-    )
+    try:
+        response = await account.get_account_by_game_name_and_tag(
+            game_name=request.json.get("game_name", ""),
+            tag_line=request.json.get("tag_line", ""),
+        )
+    except Exception:
+        return json({"error": "계정을 찾을 수 없습니다."}, status=404)
 
     puuid = response["puuid"]
 
-    if not response:
-        return json({"error": "Account not found"}, status=404)
+    try:
+        summoner_info = await summoner.get_summoner_by_puuid(puuid=puuid)
+    except Exception:
+        return json({"error": "소환사를 찾을 수 없습니다."}, status=404)
 
-    summoner_info = await summoner.get_summoner_by_puuid(puuid=puuid)
-
-    res = await league.get_league_entries_by_puuid(puuid=puuid)
+    res: Sequence[Mapping[str, Any]] = await league.get_league_entries_by_puuid(
+        puuid=puuid
+    )
     if not res:
-        return json({"error": "League entries not found"}, status=404)
+        res = [
+            {"tier": "UNRANKED", "rank": "", "leaguePoints": 0, "wins": 0, "losses": 0}
+        ]
 
     return json(
         {
@@ -66,6 +73,9 @@ def create_app(config: ServerConfig) -> Server:
     app.add_route(get_info, "/info", methods=["POST"])
     app.static("/profileicon", "riot/profileicon", name="profileicon")
     app.static("/tier", "riot/tier", name="tier")
+    app.static("/", "riot/dist/index.html", name="static")
+    app.static("/assets", "riot/dist/assets", name="assets")
+    app.static("/vite.svg", "riot/dist/vite.svg", name="favicon")
     app.before_server_start(setup_api)
     app.config.update(config)
     return app
